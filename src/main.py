92ebcf97fa065b172057408b208b94ac4f185f07
Thor Labrum <10851996@uvu.edu>
2024-10-29 10:31:42 -0600

    Update menu

--- src/main.py
+++ src/main.py
@@ -7,15 +7,23 @@ def main():
     choice = input("Choose an Item: \n 1. Add Item \n 2. Delete Item \n 3. Edit Item \n 4. Filter Items \n 5. Mark Item as complete \n")
     match choice:
     case '1': 
-      print("add")
+      title = input("enter a title: \n")
+      description = input("enter a description: \n")
+      due_date = input("enter a due date: \n")
+      task_manager.add_task(title, description, due_date)
     case '2': 
-      print("delete")
+      dead_task = input("enter title of task to delete \n")
+      task_manager.delete_task(dead_task)
     case '3': 
-      print("Edit")
+      title = input("enter a title: \n")
+      description = input("enter a description: \n")
+      due_date = input("enter a due date: \n")
+      task_manager.edit_task(title, description, due_date)
     case '4': 
-      print("filter")
+      task_manager.filter_tasks()
     case '5': 
-      print("complete")
+      task = input("enter title of task to mark complete \n")
+      task_manager.mark_task_as_completed(task)
     case _:
       print("invalid option")
 
