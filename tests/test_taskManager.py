95794f5bd53670051ace901fd4c30ae0c3f51274
Stephan Anderson <stephan84062@gmail.com>
2024-11-19 10:55:16 -0700

    Tests

--- tests/test_taskManager.py
+++ tests/test_taskManager.py
@@ -1,2 +1,21 @@
 import pytest
-import taskManager
+from src import taskManager
+
+
+
+def test_add_task():
+    manager = taskManager.TaskManager()
+    manager.add_task('Alpha', 'Beta', 'Now')
+    manager.add_task('Gamma', 'Omega', 'Then')
+    assert len(manager.tasks) == 2
+    manager.add_task('Theta', 'Phi', 'Later')
+    assert len(manager.tasks) == 3
+
+def test_find_task_by_title():
+    manager = taskManager.TaskManager()
+    manager.add_task('Alpha', 'Beta', 'Now')
+    manager.add_task('Gamma', 'Omega', 'Then')
+    #test exist
+    assert manager.find_task_by_title('Gamma').description == 'Omega'
+    #test not exist
+    assert manager.find_task_by_title('Zeta') is None
